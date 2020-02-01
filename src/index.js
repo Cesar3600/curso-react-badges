1a45f40a27b5ef7ee1281cf58085b9b25471b0cd
cesar <cesar.contreras.medina.3600@gmail.com>
2020-01-31 19:24:36 -0500

    creando componente y rendereandolo

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css';
 
-import './global.css';
-import App from './components/App';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import 'bootstrap/dist/css/bootstrap.css';
 
-const container = document.getElementById('app');
+// import './global.css';
+// import App from './components/App';
 
-ReactDOM.render(<App />, container);
+// const container = document.getElementById('app');
+
+// ReactDOM.render(<App />, container);
+
+
+
+////////////////////////////////////////////////////
+// const app = document.getElementById('app')
+
+// const elemento = document.createElement('h1')
+// elemento.innerText = 'hola mundo!'
+// app.appendChild(elemento)
+
+////////////////////////////////////////////////////
+
+//como react
+
+import React from 'react'
+import {render} from 'react-dom'
+
+const mensaje= <h1>Hola desde React!!!</h1>
+const ubicacion = document.getElementById('app')
+
+render(mensaje,ubicacion)
+
