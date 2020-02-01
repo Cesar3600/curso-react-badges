96f3d11ecdd5790139a16be9166158431a104b28
cesar <cesar.contreras.medina.3600@gmail.com>
2020-02-01 09:15:35 -0500

    agregado avatar

--- src/components/badge.js
+++ src/components/badge.js
@@ -1,15 +1,18 @@
 import React,{Component} from 'react'
 import imagensrc from '../images/badge-header.svg'
 
+const avatarIcon = 'https://www.gravatar.com/avatar?d=identicon'
+
+
 class Badge extends Component{
   render(){
     return(
       <div>
         <div>
-          <img src={imagensrc}/>
+          <img src={imagensrc} alt='header'/>
         </div>
         <div>
-          <img src='' alt='avatar'/>
+          <img src={avatarIcon} alt='avatar'/>
           <h1>Cesar<br/>Contreras</h1>
         </div>
         <div>
